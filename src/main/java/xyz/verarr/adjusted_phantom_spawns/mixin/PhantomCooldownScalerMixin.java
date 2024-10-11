e6ad8488ae10be7c9d0c27c07672917fa3b0c17d
verarr <verarr@fvnki.town>
2024-10-11 23:31:27 +0200

    Split mixins (#6)
    * split off rest statistic scaling
    * rename original mixin
    
    * add missing mixin to config
    * i forgor to store the world :skull:

---
+++ src/main/java/xyz/verarr/adjusted_phantom_spawns/mixin/PhantomCooldownScalerMixin.java
@@ -0,0 +1,44 @@
+package xyz.verarr.adjusted_phantom_spawns.mixin;
+
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.spawner.PhantomSpawner;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xyz.verarr.adjusted_phantom_spawns.AdjustedPhantomSpawns;
+import xyz.verarr.adjusted_phantom_spawns.config.AdjustedPhantomSpawnsConfig;
+
+@Mixin(PhantomSpawner.class)
+public class PhantomCooldownScalerMixin {
+    @Shadow private int cooldown;
+
+    @Unique
+    private ServerWorld adjusted_phantom_spawns$PhantomCooldownScalerMixin$serverWorld;
+
+    @Inject(method = "spawn(Lnet/minecraft/server/world/ServerWorld;ZZ)I", at = @At("HEAD"))
+    private void storeServerWorld(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals,
+                                  CallbackInfoReturnable<Integer> cir) {
+        adjusted_phantom_spawns$PhantomCooldownScalerMixin$serverWorld = world;
+    }
+
+    @Redirect(method = "spawn(Lnet/minecraft/server/world/ServerWorld;ZZ)I",
+            at = @At(value = "FIELD",
+                    target = "Lnet/minecraft/world/spawner/PhantomSpawner;cooldown:I",
+                    opcode = Opcodes.PUTFIELD, ordinal = 1))
+    private void redirectCooldownAssignment(PhantomSpawner instance, int value) {
+        int origRandValue = value / 20 - 60;
+        int percentage = adjusted_phantom_spawns$PhantomCooldownScalerMixin$serverWorld.getGameRules()
+                .getInt(AdjustedPhantomSpawns.PHANTOM_SPAWNING_COOLDOWN_PERCENTAGE);
+        float scalar = (float) percentage / 100;
+        int increment = Math.round((60 + origRandValue) * 20 * scalar);
+        this.cooldown += increment;
+        if (AdjustedPhantomSpawnsConfig.debug_print_cooldown)
+            AdjustedPhantomSpawns.LOGGER.info("Cooldown incremented from {} by {} to {} (by {}%; original {})",
+                    this.cooldown - increment, increment, this.cooldown, percentage, value);
+    }
+}
--- src/main/java/xyz/verarr/adjusted_phantom_spawns/mixin/RestStatScalerMixin.java
+++ src/main/java/xyz/verarr/adjusted_phantom_spawns/mixin/RestStatScalerMixin.java
@@ -3,60 +3,39 @@ package xyz.verarr.adjusted_phantom_spawns.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.spawner.PhantomSpawner;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import net.minecraft.util.math.random.Random;
 import xyz.verarr.adjusted_phantom_spawns.AdjustedPhantomSpawns;
 import xyz.verarr.adjusted_phantom_spawns.config.AdjustedPhantomSpawnsConfig;
 
 import java.util.Iterator;
 
 @Mixin(PhantomSpawner.class)
-public class PhantomSpawnerMixin {
-    @Shadow private int cooldown;
-
+public class RestStatScalerMixin {
     @Unique
-    private ServerWorld adjusted_phantom_spawns$serverWorld;
+    private ServerWorld adjusted_phantom_spawns$RestStatScalerMixin$serverWorld;
     @Unique
-    private ServerPlayerEntity adjusted_phantom_spawns$serverPlayerEntity;
+    private ServerPlayerEntity adjusted_phantom_spawns$RestStatScalerMixin$serverPlayerEntity;
 
     @Inject(method = "spawn(Lnet/minecraft/server/world/ServerWorld;ZZ)I", at = @At("HEAD"))
     private void storeServerWorld(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals,
                                   CallbackInfoReturnable<Integer> cir) {
-        adjusted_phantom_spawns$serverWorld = world;
+        adjusted_phantom_spawns$RestStatScalerMixin$serverWorld = world;
     }
 
     @Inject(method = "spawn(Lnet/minecraft/server/world/ServerWorld;ZZ)I",
-    at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;getStatHandler()Lnet/minecraft/stat/ServerStatHandler;"),
-    locals = LocalCapture.CAPTURE_FAILSOFT)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;getStatHandler()Lnet/minecraft/stat/ServerStatHandler;"),
+            locals = LocalCapture.CAPTURE_FAILSOFT)
     private void storePlayer(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals,
                              CallbackInfoReturnable<Integer> cir, Random random, int i,
                              Iterator<ServerPlayerEntity> var6, ServerPlayerEntity serverPlayerEntity) {
-        adjusted_phantom_spawns$serverPlayerEntity = serverPlayerEntity;
-    }
-
-    @Redirect(method = "spawn(Lnet/minecraft/server/world/ServerWorld;ZZ)I",
-            at = @At(value = "FIELD",
-                    target = "Lnet/minecraft/world/spawner/PhantomSpawner;cooldown:I",
-                    opcode = Opcodes.PUTFIELD, ordinal = 1))
-    private void redirectCooldownAssignment(PhantomSpawner instance, int value) {
-        int origRandValue = value / 20 - 60;
-        int percentage = adjusted_phantom_spawns$serverWorld.getGameRules()
-                .getInt(AdjustedPhantomSpawns.PHANTOM_SPAWNING_COOLDOWN_PERCENTAGE);
-        float scalar = (float) percentage / 100;
-        int increment = Math.round((60 + origRandValue) * 20 * scalar);
-        this.cooldown += increment;
-        if (AdjustedPhantomSpawnsConfig.debug_print_cooldown)
-            AdjustedPhantomSpawns.LOGGER.info("Cooldown incremented from {} by {} to {} (by {}%; original {})",
-                    this.cooldown - increment, increment, this.cooldown, percentage, value);
+        adjusted_phantom_spawns$RestStatScalerMixin$serverPlayerEntity = serverPlayerEntity;
     }
 
     @ModifyExpressionValue(
@@ -68,12 +47,13 @@ public class PhantomSpawnerMixin {
     private int scaleRestStatistic(int original) {
         int scaled = Math.round(original * (
                 (float) AdjustedPhantomSpawns.DEFAULT_PHANTOM_SPAWNING_THRESHOLD
-                    / (float) adjusted_phantom_spawns$serverWorld
+                        / (float) adjusted_phantom_spawns$RestStatScalerMixin$serverWorld
                         .getGameRules().getInt(AdjustedPhantomSpawns.PHANTOM_SPAWNING_THRESHOLD)
         ));
         if (AdjustedPhantomSpawnsConfig.debug_print_rest_since)
             AdjustedPhantomSpawns.LOGGER.info("Sleep statistic for {} scaled from {} to {}",
-                    adjusted_phantom_spawns$serverPlayerEntity.getName().toString(), original, scaled);
+                    adjusted_phantom_spawns$RestStatScalerMixin$serverPlayerEntity.getName().toString(),
+                    original, scaled);
         return scaled;
     }
 }
