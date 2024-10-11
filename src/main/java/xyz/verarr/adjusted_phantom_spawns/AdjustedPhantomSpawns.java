d6aeb23cbf7e9c68e858433f1ddb210baefa7905
verarr <verarr@fvnki.town>
2024-10-11 15:31:36 +0200

    initial commit
    with added gamerule

---
+++ src/main/java/xyz/verarr/adjusted_phantom_spawns/AdjustedPhantomSpawns.java
@@ -0,0 +1,26 @@
+package xyz.verarr.adjusted_phantom_spawns;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdjustedPhantomSpawns implements ModInitializer {
+	public static final String MOD_ID = "adjusted-phantom-spawns";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final int DEFAULT_PHANTOM_SPAWNING_THRESHOLD = 72000;
+	public static final GameRules.Key<GameRules.IntRule> PHANTOM_SPAWNING_THRESHOLD =
+			GameRuleRegistry.register("phantomSpawningThreshold",
+					GameRules.Category.SPAWNING,
+					GameRuleFactory.createIntRule(DEFAULT_PHANTOM_SPAWNING_THRESHOLD));
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
