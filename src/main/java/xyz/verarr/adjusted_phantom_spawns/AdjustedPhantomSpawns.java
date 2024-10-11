0b191542443ccef649995765b4b111df3ef68702
verarr <verarr@fvnki.town>
2024-10-11 20:41:18 +0200

    add cooldown percentage gamerule

--- src/main/java/xyz/verarr/adjusted_phantom_spawns/AdjustedPhantomSpawns.java
+++ src/main/java/xyz/verarr/adjusted_phantom_spawns/AdjustedPhantomSpawns.java
@@ -14,10 +14,15 @@ public class AdjustedPhantomSpawns implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public static final int DEFAULT_PHANTOM_SPAWNING_THRESHOLD = 72000;
+	public static final int DEFAULT_PHANTOM_SPAWNING_COOLDOWN_PERCENTAGE = 100;
 	public static final GameRules.Key<GameRules.IntRule> PHANTOM_SPAWNING_THRESHOLD =
 			GameRuleRegistry.register("phantomSpawningThreshold",
 					GameRules.Category.SPAWNING,
 					GameRuleFactory.createIntRule(DEFAULT_PHANTOM_SPAWNING_THRESHOLD));
+	public static final GameRules.Key<GameRules.IntRule> PHANTOM_SPAWNING_COOLDOWN_PERCENTAGE =
+			GameRuleRegistry.register("phantomSpawningCooldownPercentage",
+					GameRules.Category.SPAWNING,
+					GameRuleFactory.createIntRule(DEFAULT_PHANTOM_SPAWNING_COOLDOWN_PERCENTAGE));
 
 	@Override
 	public void onInitialize() {
