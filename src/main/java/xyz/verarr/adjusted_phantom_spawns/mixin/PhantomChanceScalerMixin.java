bf7a198fff5f5d620e1553e233de781a3265d6db
verarr <verarr@fvnki.town>
2024-10-18 19:06:34 +0200

    Use a slice for chance scaler mixin (#10)

--- src/main/java/xyz/verarr/adjusted_phantom_spawns/mixin/PhantomChanceScalerMixin.java
+++ src/main/java/xyz/verarr/adjusted_phantom_spawns/mixin/PhantomChanceScalerMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.verarr.adjusted_phantom_spawns.AdjustedPhantomSpawns;
 import xyz.verarr.adjusted_phantom_spawns.config.AdjustedPhantomSpawnsConfig;
@@ -27,9 +28,11 @@ public class PhantomChanceScalerMixin {
         adjusted_phantom_spawns$PhantomChanceScalerMixin$serverWorld = world;
     }
 
-    @WrapOperation(method = "spawn(Lnet/minecraft/server/world/ServerWorld;ZZ)I",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I",
-                    ordinal = 1))
+    @WrapOperation(
+            method = "spawn(Lnet/minecraft/server/world/ServerWorld;ZZ)I",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I", ordinal = 0),
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/stat/StatType;getOrCreateStat(Ljava/lang/Object;)Lnet/minecraft/stat/Stat;"))
+    )
     private int scaleRandomInput(Random random, int j, Operation<Integer> original) {
         float scalar = (float) adjusted_phantom_spawns$PhantomChanceScalerMixin$serverWorld.getGameRules().getInt(AdjustedPhantomSpawns.PHANTOM_SPAWNING_CHANCE_PERCENTAGE)
                 / AdjustedPhantomSpawns.DEFAULT_PHANTOM_SPAWNING_CHANCE_PERCENTAGE;
